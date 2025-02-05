7477c8dc68d39098dbdaef655a5a3448d50ce04f
fadwa <fadwamhmd258@gmail.com>
2025-02-05 04:05:30 +0200

    config file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,12 @@
-// module.exports = {
-//   apps: [
-//     {
-//       name: "stephan-app",
-//       script: "app.js",
-//       node_args: "--experimental-modules",
-//       env: {
-//         NODE_ENV: "production",
-//       },
-//     },
-//   ],
-// };
+module.exports = {
+  apps: [
+    {
+      name: "stephan-app",
+      script: "app.js",
+      node_args: "--experimental-modules",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};

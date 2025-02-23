886c92c0b2c904db2f609c29ef1d9bbaf6862464
Hamza Alaydi <72568157+HamzAlaydi@users.noreply.github.com>
2025-02-23 15:26:39 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect("mongodb+srv://hamzaalaydi99:Hhaammzzaa2001@cluster0.vp5rr.mongodb.net/presentation", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });

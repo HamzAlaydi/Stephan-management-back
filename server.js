90a50e6a613b121b437fbb6d82cc76a73634bf94
Hamza Alaydi <72568157+HamzAlaydi@users.noreply.github.com>
2025-02-23 16:55:18 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-// server.js - Entry Point
-const dotenv = require('dotenv');
-const app = require('./app');
-const connectDB = require('./config/db');
-const serverless = require("serverless-http"); // Add this
+const dotenv = require("dotenv");
+const app = require("./app");
+const connectDB = require("./config/db");
+const serverless = require("serverless-http");
 
 // Load environment variables
 dotenv.config();
@@ -10,6 +9,5 @@ dotenv.config();
 // Connect to the database
 connectDB();
 
-// Start server
-const PORT = process.env.PORT || 5000;
+// Export the handler for Lambda
 module.exports.handler = serverless(app);

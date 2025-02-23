8d87fa4cc17bb08270520d2cc07de7205dac9ee8
hamza <hamza.alaydi.99@gmail.com>
2025-02-23 18:46:52 +0200

    try to upload node_modules

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,30 @@ const nodeExternals = require('webpack-node-externals');
 const path = require('path');
 
 module.exports = {
-  entry: "./server.js",
-  target: "node",
-  externals: [nodeExternals()],
+  entry: "./server.js", // Entry point of your app
+  target: "node", // Target Node.js environment
+  externals: [nodeExternals()], // Exclude node_modules from the bundle
   output: {
-    libraryTarget: "commonjs2",
-    path: path.resolve(__dirname, ".webpack"),
-    filename: "handler.js",
+    libraryTarget: "commonjs2", // Output format for Lambda
+    path: path.resolve(__dirname, ".webpack"), // Output directory
+    filename: "handler.js", // Output file name
   },
-  mode: "production",
+  mode: "production", // Optimize for production
   optimization: {
-    minimize: true,
+    minimize: true, // Minimize the bundle
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // Process .js files
+        exclude: /node_modules/, // Exclude node_modules
+        use: {
+          loader: "babel-loader", // Use Babel for transpilation
+          options: {
+            presets: ["@babel/preset-env"], // Use Babel preset for Node.js
+          },
+        },
+      },
+    ],
   },
 };

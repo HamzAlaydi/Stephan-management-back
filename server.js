1ad504de58e5734c960c3569c69d6ffc1cd14413
hamza <hamza.alaydi.99@gmail.com>
2025-02-23 13:04:31 +0200

    push CI/CD

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const dotenv = require('dotenv');
 const app = require('./app');
 const connectDB = require('./config/db');
+const serverless = require("serverless-http"); // Add this
 
 // Load environment variables
 dotenv.config();
@@ -11,4 +12,4 @@ connectDB();
 
 // Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports.handler = serverless(app);

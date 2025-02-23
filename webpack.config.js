faa4f2718bc15f798f1789cc4e6889bf26b6e0de
Hamza Alaydi <72568157+HamzAlaydi@users.noreply.github.com>
2025-02-23 16:55:53 +0200

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const nodeExternals = require('webpack-node-externals');
+const path = require('path');
+
+module.exports = {
+  entry: './server.js',
+  target: 'node',
+  externals: [nodeExternals()],
+  output: {
+    libraryTarget: 'commonjs2',
+    path: path.resolve(__dirname, '.webpack'),
+    filename: 'handler.js'
+  },
+  mode: 'production',
+  optimization: {
+    minimize: true
+  }
+};

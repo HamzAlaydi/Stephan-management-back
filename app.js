1220dfb392c996246dddbc3dea9d19431bbe4816
Hamza Alaydi <72568157+HamzAlaydi@users.noreply.github.com>
2025-02-23 16:55:11 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
-// app.js - Application Configuration
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const routes = require("./routes");
-const { errorHandler, notFound } = require("./middleware/errorMiddleware.js");
+const { errorHandler, notFound } = require("./middleware/errorMiddleware");
 
 const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); // Parse URL-encoded form data
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
 // API Routes
 app.use("/api", routes);
+
+// Root route
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
+
 // Error handling middleware
 app.use(notFound);
 app.use(errorHandler);

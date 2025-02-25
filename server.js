d93144fe0f714739d15f519ffafccec098e0a8a6
hamza <hamza.alaydi.99@gmail.com>
2025-02-25 15:16:47 +0200

    Removed .env from Git tracking

--- server.js
+++ server.js
@@ -10,4 +10,9 @@ dotenv.config();
 connectDB();
 
 // Export the handler for Lambda
-module.exports.handler = serverless(app);
+// module.exports.handler = serverless(app);
+
+// app.listen
+app.listen(process.env.PORT, () => {
+  console.log(`server runnint on port  ${process.env.PORT}`);
+});
